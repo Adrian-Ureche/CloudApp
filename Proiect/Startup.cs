4bd55101731ec0a9268a4c9f8135b1a715d21d1f
Christian99999 <christian.andreas1999@gmail.com>
2021-12-10 00:13:37 +0200

    Update Startup.cs

--- Proiect/Startup.cs
+++ Proiect/Startup.cs
@@ -21,6 +21,9 @@ namespace Proiect
             services.AddScoped<IUsersRepository, UsersRepository>();
             services.AddScoped<IProblemsRepository, ProblemsRepository>();
             services.AddControllers();
+
+            services.AddCors(options => options.AddDefaultPolicy(
+                builder => builder.AllowAnyOrigin()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +37,8 @@ namespace Proiect
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
